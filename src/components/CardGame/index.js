8f07793bd95b3611cee2ab15cf173749a5c75ec4
Victorprog4 <victroeeee4862@gmail.com>
2022-11-12 15:25:00 -0300

    criando components

---
+++ src/components/CardGame/index.js
@@ -0,0 +1,11 @@
+import "./style.css";
+
+function CardGame() {
+    return`
+    <article class="card-game" >
+    <img src="images/alura-pixel.png" alt="">
+    </article>
+    `;
+}
+
+export default CardGame;
